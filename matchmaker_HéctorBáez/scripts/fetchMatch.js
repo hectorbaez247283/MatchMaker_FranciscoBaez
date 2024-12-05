566330ad0839fc0d75f1e60095f5c27e23a5a96c
hectorbaez247283 <159949506+hectorbaez247283@users.noreply.github.com>
2024-12-04 23:55:30 -0700

    Práctica lista

--- matchmaker_HéctorBáez/scripts/fetchMatch.js
+++ matchmaker_HéctorBáez/scripts/fetchMatch.js
@@ -1,20 +1,20 @@
 window.onload = function () {
 
-    cargarInfo();
-    const data = recuperarInfo();
+  cargarInfo();
+  const data = recuperarInfo();
 }   
 
 function recuperaInfo() {
-    return JSON.parse(localStorage.getItem("match"));
+  return JSON.parse(localStorage.getItem("match"));
 }
 
-
-function cargarInfo() {
+async function cargarInfoAsync() {
+try{
   const query = window.location.search;
   const urlParams = new URLSearchParams(query);
 
-  const nac = urlParams.get("nac");
-  const genero = urlParams.get("genero");
+  const nac= urlParams.get("nac");
+  const genero= urlParams.get("genero");
 
   const request = new Request(
     `https://randomuser.me/api/?nat=${nac}&gender=${genero}`,
@@ -26,42 +26,70 @@ function cargarInfo() {
     }
   );
 
+  const response= await fetch(request);
+  const data= await response.json();
+  llenarInfo(data.result[0]);
+
+}catch(error){
+  console.log(error);
+  alert("Hubo un error encontrado a tu otra mitad :(")
+}
+
+}
+
+function cargarInfo() {
+const query = window.location.search;
+const urlParams = new URLSearchParams(query);
+
+const nac = urlParams.get("nac");
+const genero = urlParams.get("genero");
+
+const request = new Request(
+  `https://randomuser.me/api/?nat=${nac}&gender=${genero}`,
+  {
+    method: "get",
+    headers: new Headers({
+      "Content-Type": "application/json",
+    })
+  }
+);
+
 fetch(request)
-  .then(function(response) {
-    return response.json();    
-  }).then(function(data){
-    console.log(data);
-    llenarInfo(data.results[0]);
-  }).catch(function(error){
-    console.log(error);
-  });
+.then(function(response) {
+  return response.json();    
+}).then(function(data){
+  console.log(data);
+  llenarInfo(data.results[0]);
+}).catch(function(error){
+  console.log(error);
+});
 }
 
 function llenarInfo(data) {
-    const img = document.getElementById("img");
-    const name = document.getElementById("name");
-    const email = document.getElementById("email");
-    const age = document.getElementById("age");
-    const country = document.getElementById("country");
-    const city = document.getElementById("city");
-    const phone = document.getElementById("phone");
-    const gender = document.getElementById("gender");
-    const dob = document.getElementById("dob");
-    const state = document.getElementById("state");
-    const cell = document.getElementById("cellphone");
-    const address = document.getElementById("address");
+  const img = document.getElementById("img");
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const age = document.getElementById("age");
+  const country = document.getElementById("country");
+  const city = document.getElementById("city");
+  const phone = document.getElementById("phone");
+  const gender = document.getElementById("gender");
+  const dob = document.getElementById("dob");
+  const state = document.getElementById("state");
+  const cell = document.getElementById("cellphone");
+  const address = document.getElementById("address");
 
-    img.src = data.picture.large;
-    name.innerHTML = `${data.name.first} ${data.name.last}`;
-    email.innerHTML = data.email;
-    dob.innerHTML = data.dob.age;
-    country.innerHTML = data.location.country;
-    city.innerHTML = data.location.city;
-    phone.innerHTML = data.phone;
-    age.innerHTML = data.dob.age;
-    email.innerHTML = data.email;
-    cell.innerHTML = data.cell;
-    address.innerHTML = `${data.location.street.number} ${data.location.street.name}`;
-    gender.innerHTML = data.gender;
-    state.innerHTML = data.location.state;
+  img.src = data.picture.large;
+  name.innerHTML = `${data.name.first} ${data.name.last}`;
+  email.innerHTML = data.email;
+  dob.innerHTML = data.dob.age;
+  country.innerHTML = data.location.country;
+  city.innerHTML = data.location.city;
+  phone.innerHTML = data.phone;
+  age.innerHTML = data.dob.age;
+  email.innerHTML = data.email;
+  cell.innerHTML = data.cell;
+  address.innerHTML = `${data.location.street.number} ${data.location.street.name}`;
+  gender.innerHTML = data.gender;
+  state.innerHTML = data.location.state;
 }
